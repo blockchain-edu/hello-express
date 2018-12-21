212ad4d2ba6b9df30f87d288ff96540a991e97cf
johnta0 <22534726+johnta0@users.noreply.github.com>
2018-12-21 17:38:46 +0900

    set response header

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,12 @@ import * as express from 'express';
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/invoice', (req, res) => {
     const address = 'muoRvov495zRtS7DDJ6eFMbd7KjF7YS7YV'; // use address generated from 
     return res.json({
